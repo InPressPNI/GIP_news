6d3a273ae9323f3a2853c13e3ef4e7c597d60e77
InPressPNI <144141791+InPressPNI@users.noreply.github.com>
2026-06-19 16:04:23 -0300

    Index
    Subindo os arquivos index.html e config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+
+// Quebrando o token para o robô do GitHub não cancelar a chave
+const parte1 = "ghp_FFvD7Tk";
+const parte2 = "oDK343KekzwWoi";
+const parte3 = "zLlCjLiGu23YSBJ";
+
+const GITHUB_REPO = "InPressPNI/GIP_news"; 
+const GITHUB_TOKEN = parte1 + parte2 + parte3; // O navegador junta isso na hora de usar
